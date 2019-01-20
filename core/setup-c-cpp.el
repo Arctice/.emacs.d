0719537b5afbe6d2c550f5771b8ec9f6e4a2fe10
arc <naurthal0@gmail.com>
2019-01-20 16:29:37 +0000

    compilation helper binding

--- core/setup-c-cpp.el
+++ core/setup-c-cpp.el
@@ -2,3 +2,15 @@
 
 ;; open .h files in C++ mode by default
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+
+;; (generated keyboard macro)
+;; runs C-p S then C-p c and confirms
+(fset 'project-save-compile
+      (lambda (&optional arg)
+        "Keyboard macro."
+        (interactive "p")
+        (kmacro-exec-ring-item (quote ([3 112 83 3 112 99 return] 0 "%d")) arg)))
+
+(add-hook 'c++-mode-hook
+          (lambda () (local-set-key (kbd "<insert>") #'project-save-compile)))
