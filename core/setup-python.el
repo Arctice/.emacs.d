cee412549364f210bdcda660e23c65b5bf2af009
arc <naurthal0@gmail.com>
2019-01-28 17:55:27 +0000

    saner elpy settings

--- core/setup-python.el
+++ core/setup-python.el
@@ -3,3 +3,18 @@
 
 (setq python-shell-interpreter "ipython3"
       python-shell-interpreter-args "-i --simple-prompt")
+
+(setq elpy-rpc-python-command "python3")
+
+;; yapf binding, fmt the entire file on tab for now
+(add-hook 'python-mode-hook
+          (lambda ()
+            (local-set-key (kbd "TAB") 'elpy-yapf-fix-code)))
+
+
+;; DON'T LET ELPY DO ELPY THINGS
+(substitute-key-definition
+ 'elpy-nav-forward-block nil elpy-mode-map)
+
+(substitute-key-definition
+ 'elpy-nav-backward-block nil elpy-mode-map)
--- custom.el
+++ custom.el
@@ -6,6 +6,9 @@
  '(custom-safe-themes
    (quote
     ("1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" default)))
+ '(elpy-modules
+   (quote
+    (elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-sane-defaults)))
  '(package-selected-packages
    (quote
     (dumb-jump dashboard disable-mouse modern-cpp-font-lock realgud lua-mode yafolding tabbar solarized-theme smex rainbow-delimiters projectile paredit neotree magit ido-completing-read+ git-gutter exec-path-from-shell elpy diminish counsel clojure-mode-extra-font-locking cider anzu))))
